c249c600be0649d39fc22c210e9fa8dc778cb189
AdityaGaur7 <adgaur027@gmail.com>
2024-12-21 16:33:44 +0530

    bday initiated

--- src/components/About/Birthday.jsx
+++ src/components/About/Birthday.jsx
@@ -32,7 +32,7 @@ const Birthday = () => {
         secondTitle="Seconds"
         endAtZero
        
-        endAt={"2024-12-20 23:59:59"} // Date/Time
+        endAt={"2025-12-19 23:59:59"} // Date/Time
         onTimeUp={() => console.log("Happy Birthday Aditya 🥳🎉🎊")}
       />
     </div>
